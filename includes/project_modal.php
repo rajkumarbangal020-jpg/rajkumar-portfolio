3ae697ee7c364e705adeba3b41b11893b36c1832
rajkumarbangal020-jpg <rajkumarbangal020@gmail.com>
2026-09-10 14:56:19 +0530

    Add project modal

---
+++ includes/project_modal.php
@@ -0,0 +1,18 @@
+<!-- Project Details Modal -->
+<div class="modal fade" id="projectDetailModal" tabindex="-1" aria-labelledby="modalProjectTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-lg">
+        <div class="modal-content modal-content-custom">
+            <div class="modal-header modal-header-custom d-flex justify-content-between align-items-center">
+                <h5 class="modal-title font-weight-bold" id="modalProjectTitle">Project Specifications</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body modal-body-custom" id="modalProjectBody">
+                <div class="text-center py-4">
+                    <div class="spinner-border text-info" role="status">
+                        <span class="visually-hidden">Loading...</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
